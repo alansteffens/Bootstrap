c81fc32f0a6f54ebbac70efb7b00149f54577229
alansteffens <asteffens@digital-vision.com>
2017-02-06 20:02:17 -0800

    Test out PHP includes
    Test out PHP includes

---
+++ header.php
@@ -0,0 +1,52 @@
+<div class="logo"><a href="#"><img src="images/logo.gif" alt=""></a></div>
+<div class="ems">
+  <img src="images/ems.png" alt=""><br>
+  <div class="tagline"><img src="images/tagline.png" alt=""></div>
+</div>
+
+<div class="menubutton">menu<br>
+  <!-- this is a good section for a PHP include -->
+  <span data-panel="panel1" class="panel-button"></span>
+  <div id="panel1" style="text-align: left;">
+    <div id="accordion" style="text-align: left;">
+      <ul>
+        <li>
+          <div>WHO WE ARE</div>
+          <ul>
+            <li><a href="companyprofile.html">COMPANY PROFILE</a></li>
+            <li><a href="ourteam.html">OUR TEAM</a></li>
+            <li><a href="locations.html">LOCATIONS</a></li>
+            <li><a href="mission.html">MISSION-VISION-VALUES</a></li>
+            <li><a href="culture.html">CULTURE</a></li>
+          </ul>
+        </li>
+        <li>
+          <div>WHAT WE DO</div>
+          <ul>
+            <li><a href="designengr.html">DESIGN &amp; ENGINEERING SERVICES</a></li>
+            <li><a href="prototypes-npi.html">PROTOTYPES &amp; NPI</a></li>
+            <li><a href="supplychain.html">SUPPLY CHAIN MANAGEMENT</a></li>
+            <li><a href="manufacturing.html">MANUFACTURING</a></li>
+            <li><a href="repair.html">REPAIR &amp; REFURBISHMENT</a></li>
+            <li><a href="quality.html">QUALITY</a></li>
+            <li><a href="fulfillment.html">FULFILLMENT</a></li>
+          </ul>
+        </li>
+        <li>
+          <div>WHY CHOOSE US</div>
+          <ul>
+            <li><a href="industry.html">INDUSTRY EXPERTISE</a></li>
+            <li><a href="technology.html">TECHNOLOGY</a></li>
+            <li><a href="corpinitiatives.html">CORPORATE INITIATIVES</a></li>
+            <li><a href="customers.html">CUSTOMER TESTIMONIALS</a></li>
+            <li><a href="awards.html">AWARDS</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul id="menu">
+        <li><div><a href="careers.html">CAREERS</a></div></li>
+        <li><div><a href="contactus.html">CONTACT US</a></div></li>
+      </ul>
+    </div> <!-- end accordian -->
+  </div> <!-- end panel -->
+</div> <!-- end menubutton -->
